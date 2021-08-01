d813d3cdca2bc233cf276c09af1e0f0568b0e144
TYROnosaur <pavelnomer1@gmail.com>
2021-08-01 17:18:04 +0200

    Small fix

---
+++ bot/cogs/utils/dbconnection.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+class Connection:
+    def __init__(self):
+        self.connection = sqlite3.connect('miracle-bot.db')
+        self.cursor = self.connection.cursor()
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS users (id serial primary key, name text NOT NULL, steamid32 varchar(9), time integer)')
+        self.connection.commit()
+
+    def getConnection(self):
+        return self.connection
+
+    def getCursor(self):
+        return self.cursor
+
+    def close(self):
+        self.connection.close()
