f937a370ee19969e0a34fb9d9a2248bd2a23951d
TYROnosaur <pavelnomer1@gmail.com>
2021-08-01 19:44:54 +0200

    Added General cog and list command

---
+++ bot/cogs/general.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+import datetime as dt
+import aiohttp
+import os
+import bot.conf as conf
+
+class General(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='list')
+    async def list_command(self, ctx):
+        embed = discord.Embed(title="List of top users")
+        s = '\n'.join(conf.connection.getCursor().execute('SELCT name, steamid32, time FROM users ORDER BY time DESC LIMIT 10;'))
+        embed.add_field(name='', value=s)
+        await ctx.send(embed=embed)
+
+    @match_command.error
+    async def list_command_error(self, ctx, exc):
+        pass
+
+def setup(bot):
+    bot.add_cog(General(bot))
