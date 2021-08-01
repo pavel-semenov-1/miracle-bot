9ff5ecda7e5b01a23c7f1970b97dcce70de290a2
TYROnosaur <pavelnomer1@gmail.com>
2021-08-01 19:01:51 +0200

    Make column `name` unique

--- bot/cogs/utils/dbconnection.py
+++ bot/cogs/utils/dbconnection.py
@@ -4,7 +4,7 @@ class Connection:
     def __init__(self):
         self.connection = sqlite3.connect('miracle-bot.db')
         self.cursor = self.connection.cursor()
-        self.cursor.execute('CREATE TABLE IF NOT EXISTS users (id serial primary key, name text NOT NULL, steamid32 varchar(9), time integer)')
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS users (id serial primary key, name text NOT NULL UNIQUE, steamid32 varchar(9), time integer)')
         self.connection.commit()
 
     def getConnection(self):
